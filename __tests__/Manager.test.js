8fcb049f28a9c574108282f4dde10a0f23b06e1d
coderchrisdean <coderchrisdean@gmail.com>
2023-04-08 18:57:07 -0700

    added Manager test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,15 @@
+const { randomUUID } = require("uuid");
+const Manager = require("../lib/Manager");
+
+test('creates a manager object', () => {
+    const manager = new Manager('Chris', 1, 'coderchrisdean@gmail.com', randomUUID);
+    expect(manager.name).toBe('Chris');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+});
+
+test('gets manager role', () => {
+    const manager = new Manager('Chris', 1, 'coderhchrisdean@gmail.com', randomUUID);
+    expect(manager.getRole()).toEqual('Manager');
+});
