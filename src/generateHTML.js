47e0c595c67dd180a01eae975cea9b8b2a6a437d
coderchrisdean <coderchrisdean@gmail.com>
2023-04-09 06:55:14 -0700

    fixed classes

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,6 +1,3 @@
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
 
 function generateHTML(teamMembers) {
   const htmlArray = [];
